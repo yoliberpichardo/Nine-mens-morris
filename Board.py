8fc7817c6f17aede8971abfff7fe668cea7a3e2f
A19B45622 educacion <A19B45622@educacion.edu.do>
2020-03-02 19:25:26 -0400

    le he hecho algunas modificaciones al tablero y tambien ha la entrada de las coordenadas

--- Board.py
+++ Board.py
@@ -11,24 +11,24 @@ class Board:
         # self.piece_white = piece.quant_piece()
         self.matriz = []
         self.view_board = ''
-        self.coordinate = "     0    1     2      3      4    5    6"
+        self.coordinate = "     0    1    2    3    4    5    6"
 
     
     def table(self):
 
-        self.board = [[ [' '],'-------------',[' '],'------------',[' '] ],
+        self.board = [[ [' '],'-----','-----',[' '],'-----','-----',[' '] ],
                                             
-                     ['  |  ', [' '],'--------', [' '],'-------',[' '],'  | '],
+                     ['  |  ', [' '],'-----', [' '],'-----',[' '],'  | '],
                             
-                     ['  |    |   ',[' '],'--',[' '],'--',[' '],'  |    |   '],
+                     ['  |    ','|  ',[' '],[' '],[' '],'  |  ','  |   '],
 
-                     [[' '],'',[' '],' ',[' '], '         ',[' '],'',[' '],'',[' ']],
+                     [[' '] ,[' '],[' '], '     ',[' '],[' '],[' ']],
 
-                     ['  |    |   ',[' '],'--',[' '],'--',[' '],'  |    |     '],                       
+                     ['  |    ','|  ',[' '],[' '],[' '],'  |   ',' |   '],                       
 
-                     ['  |  ',[' '],'--------', [' '],'-------',[' '],'  | '],
+                     ['  |  ', [' '],'-----', [' '],'-----',[' '],'  | '],
 
-                    [ '',[' '],'-------------',[' '],'------------',[' '] ]     ] 
+                     [ [' '],'-----','-----',[' '],'-----','-----',[' '] ]     ] 
 
 
     
@@ -45,16 +45,16 @@ class Board:
         return self.view_board
     
     def input_coordinate(self,player):
-        row_input = input('enter row: ')
-        column_input =input('enter column: ')
-        check = '0123456'
+        row_input = str(input('enter row: '))
+        column_input =str(input('enter column: '))
+        checks ='0123456' #[0,1,2,3,4,5,6]
+        for check in  checks:
+            if row_input not in checks or column_input not in checks and row_input == '' or column_input == '':
+                print('solo se pueden introducir numeros, de 0 al 6')
+                row_input = input('enter row: ')
+                column_input =input('enter column: ')
+        self.matriz = self.board[int(row_input)][int(column_input)] 
 
-        while row_input or column_input not in check and row_input or column_input == '':
-            print('no se puede introducir letras, solo numeros del 0 al 6')
-            row_input = input('enter row: ')
-            column_input = input('enter column: ')
-
-        self.matriz = self.board[int(row_input)][int(column_input)]
         if self.player.quant <= 9 :
             while self.player.quant != 0:
                 if isinstance(self.matriz,list):
@@ -65,7 +65,7 @@ class Board:
                     print('no se puede colocar aqui vuelva ha introducir las coordenadas')
                     row_input = input('enter row: ')
                     column_input =input('enter column: ')
-                    self.matriz = self.board[row_input][column_input]
+                    self.matriz = self.board[int(row_input)][int(column_input)] 
             return self.board
    
 
