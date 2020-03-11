e67976caa0c2fe1a9fb80fb68883cf93a1f6596e
yoliber <yoliberfelixpp01@gmail.com>
2020-03-11 14:39:04 -0400

    we have simplified the player class and some functions, we also did the validation in case there is a busy box

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from Board import *
 from players import *
 
-play = Player('')
 print('Welcome to the famous game NINE MENS MORRIS !!'.upper())
 print('The gameplay is: 1 → player vs boot, 2 → player vs player'.upper())
 
@@ -9,9 +8,9 @@ input('Press Enter...')
 
 
 
-print(mapper_tokens(play))
+print(mapper_tokens())
 
-table_p = Board(play.color_white)
+table_p = Board(playerList[0].colorTokens)
 table_p.table()
-table_p.run_table(play)
-print(table_p.view(player))
+table_p.run_table()
+print(table_p.view())
