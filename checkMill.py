fe33ad0f004393cf0da4f038a488159cf6bd43b9
thejer5 <thejer19@gmail.com>
2020-07-21 10:48:46 -0400

    we are working on optimizing the mill system

--- checkMill.py
+++ checkMill.py
@@ -13,40 +13,44 @@ matriz = [[ [' '],'-----','-----',[' '],'-----','-----',[' '] ],#0m
         [[' '], '-----', '-----', [' '], '-----', '-----', [' ']]]#6m
 
 
-def ranget(x, y, a, b):
-    if b + 2 > y:
+def ranget(a, b, x, y):
+    if y + 4 > a:
+        return False
+    elif  y + 3 > a:
+        return False
+    elif y - 3 < 0:
         return False
     else:
         return True
 
-def Mill_matriz(matriz):
+
+
+def Matriz(matriz):
     for x in range(7):
         for y in range(7):
-            if matriz[x][y] and ranget(7, 7, x, y):
-                if matriz[x][y] == ['0'] and matriz[x][y+1] == ['0']:#f--4
+            if matriz[x][y] == ['0'] and ranget(7, 7, x, y):
+                if matriz[x][y] == ['0'] and matriz[x][y+3] == ['0']:#0m, 6m
+                    if matriz[x][y-3] == ['0']:
+                        print('logrado', end=' ')
+
+                if matriz[x][y] == ['0'] and matriz[x][y+2] == ['0']:#1m, 5m
+                    if matriz[x][y-2] == ['0']:
+                        print('logrado', end=' ')
+
+                if matriz[x][y] == ['0'] and matriz[x][y+1] == ['0']:#f -- 4
                     if matriz[x][y-1] == ['0']:
-                        return True
+                        print('logrado', end=' ')
 
-    if matriz[0][0] == ['0'] and matriz[0][0+3] == ['0']:#fila 0m
-        if matriz[0][3+3] == ['0']:
-            return True
 
-    if matriz[1][1] == ['0'] and matriz[1][1+2] == ['0']:#fila 1m
-        if matriz[1][3+2] == ['0']:
-            return True
-    
-    if matriz[5][1] == ['0'] and matriz[5][1+2] == ['0']:#5m
-        if matriz[5][3+2] == ['0']:
-            return True
+            else:
+                print('no', end = ' ')
 
-    if matriz[6][0] == ['0'] and matriz[6][0+3] == ['0']:#6m
-        if matriz[6][3+3] == ['0']:
-            print('logrado', end=' ')
 
-    else:
-        return False
 
-print(Mill_matriz(matriz))
+
+  
+
+Matriz(matriz)
 
 
 
